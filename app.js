d6d3de6e3139b4025f92477760b52bff77c96fec
Waggerok <115732069+Waggerok@users.noreply.github.com>
2025-01-13 11:07:46 +0300

    static

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const PORT = process.env.PORT || 5000;
 //App
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'uploads/images')))
 app.use('/uploads/images', express.static(path.join(__dirname, 'uploads/images')));
 app.use('/uploads/models', express.static(path.join(__dirname, 'uploads/models')));
 
